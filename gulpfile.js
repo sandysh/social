de21ce0a4141fb20a4fc002927c2c5c8b01fd28e
sandesh <sandeshsatyal@gmail.com>
2016-11-28 13:35:52 +0545

    migrated to laravel elixir gulp webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 const elixir = require('laravel-elixir');
 
-require('laravel-elixir-vueify');
-
-require('laravel-elixir-browserify-official');
+require('laravel-elixir-vue-2');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,11 +9,10 @@ require('laravel-elixir-browserify-official');
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
  | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
+ | file for your application as well as publishing vendor resources.
  |
  */
 
-elixir(function(mix) {
-    mix.browserify('app.js');
-    //elixir.config.registerWatcher("default", "resources/**");
+elixir((mix) => {
+    mix.webpack('app.js');
 });
